32bef5f088b37a2a52138c86e222e0b97d735fe3
zamcham <cristian@zamcham.com>
2023-03-06 15:09:17 -0500

    Add background hide and show function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navBarLinks = document.getElementsByClassName('menu-links')[0];
+const dimBackground = document.getElementsByClassName('dim-background')[0];
+
 
 toggleButton.addEventListener('click', () => {
-  navBarLinks.classList.toggle('active')
+  navBarLinks.classList.toggle('active');
+  dimBackground.classList.toggle('active');
 })
