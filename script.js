14c811dfe4f3bce77c38e3288cbf0446f739617f
zamcham <cristian@zamcham.com>
2023-03-06 15:56:35 -0500

    Update JS according to linters requirements

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const navBarLinks = document.getElementsByClassName('menu-links')[0];
 const dimBackground = document.getElementsByClassName('dim-background')[0];
 const exitMobileButton = document.getElementsByClassName('exit-mobile-menu')[0];
 
-
 toggleButton.addEventListener('click', () => {
   navBarLinks.classList.toggle('active');
   dimBackground.classList.toggle('active');
   toggleButton.classList.toggle('active');
   exitMobileButton.classList.toggle('active');
-})
+});
 
 exitMobileButton.addEventListener('click', () => {
   navBarLinks.classList.toggle('active');
   dimBackground.classList.toggle('active');
   toggleButton.classList.toggle('active');
   exitMobileButton.classList.toggle('active');
-})
+});
