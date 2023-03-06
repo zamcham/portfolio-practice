eecfc63d8aff7e1a928682f126a453211b8f4483
zamcham <cristian@zamcham.com>
2023-03-06 15:28:57 -0500

    Add "x" exit button to mobile menu

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navBarLinks = document.getElementsByClassName('menu-links')[0];
 const dimBackground = document.getElementsByClassName('dim-background')[0];
+const exitMobileButton = document.getElementsByClassName('exit-mobile-menu')[0];
 
 
 toggleButton.addEventListener('click', () => {
   navBarLinks.classList.toggle('active');
   dimBackground.classList.toggle('active');
+  toggleButton.classList.toggle('active');
+  exitMobileButton.classList.toggle('active');
+})
+
+exitMobileButton.addEventListener('click', () => {
+  navBarLinks.classList.toggle('active');
+  dimBackground.classList.toggle('active');
+  toggleButton.classList.toggle('active');
+  exitMobileButton.classList.toggle('active');
 })
