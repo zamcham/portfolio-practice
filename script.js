4580c53e69be8da2b30c7f5c8a9df4780b24b739
zamcham <cristian@zamcham.com>
2023-03-06 19:13:58 -0500

    Update event listener on menu links

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navBarLinks = document.getElementsByClassName('menu-links')[0];
 const dimBackground = document.getElementsByClassName('dim-background')[0];
 const exitMobileButton = document.getElementsByClassName('exit-mobile-menu')[0];
+const menuTabs = document.getElementsByClassName('menutabs');
 
 toggleButton.addEventListener('click', () => {
   navBarLinks.classList.toggle('active');
@@ -16,3 +17,12 @@ exitMobileButton.addEventListener('click', () => {
   toggleButton.classList.toggle('active');
   exitMobileButton.classList.toggle('active');
 });
+
+for (var i = 0; i < menuTabs.length; i++) {
+  menuTabs[i].addEventListener('click', () => {
+    navBarLinks.classList.toggle('active');
+    dimBackground.classList.toggle('active');
+    toggleButton.classList.toggle('active');
+    exitMobileButton.classList.toggle('active');
+  });
+}
