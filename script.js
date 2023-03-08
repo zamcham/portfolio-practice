9f2137c0d0f8ec13bbf46b415fe1d1d9ad20a9dc
zamcham <cristian@zamcham.com>
2023-03-08 13:39:18 -0500

    Add projects array of objects

--- script.js
+++ script.js
@@ -18,3 +18,62 @@ for (let i = 0; i < menuTabs.length; i += 1) {
   menuTabs[i].addEventListener('click', toggleActiveClass);
 }
 // #endregion
+
+// #region projects data
+const projectsList = [
+  {
+    name: 'My First Project in List',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    feturedImageMobile: 'mobilepeojectpreview.jpg',
+    feauturedImageDesktop: 'desktopSnapshot.svg',
+    livelink: 'https://www.google.com/',
+    sourcelink: '#',
+  },
+  {
+    name: 'My Second Project in List',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    feturedImageMobile: 'cardplaceholder.png',
+    feauturedImageDesktop: 'cardplaceholder.png',
+    livelink: '#',
+    sourcelink: '#',
+  },
+  {
+    name: 'My Third Project in List',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    feturedImageMobile: 'cardplaceholder.png',
+    feauturedImageDesktop: 'cardplaceholder.png',
+    livelink: '#',
+    sourcelink: '#',
+  },
+  {
+    name: 'My Fourth Project in List',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    feturedImageMobile: 'cardplaceholder.png',
+    feauturedImageDesktop: 'cardplaceholder.png',
+    livelink: '#',
+    sourcelink: '#',
+  },
+  {
+    name: 'My Fifth Project in List',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    feturedImageMobile: 'cardplaceholder.png',
+    feauturedImageDesktop: 'cardplaceholder.png',
+    livelink: '#',
+    sourcelink: '#',
+  },
+  {
+    name: 'My Sixth Project in List',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    feturedImageMobile: 'cardplaceholder.png',
+    feauturedImageDesktop: 'cardplaceholder.png',
+    livelink: '#',
+    sourcelink: '#',
+  },
+];
+// #endregion
