d6fcf03cefdc83eab63bd1bdbbcad6295d8b0bba
zamcham <cristian@zamcham.com>
2023-03-08 13:34:33 -0500

    Mobile menu

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
+// #region Mobile Menu
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navBarLinks = document.getElementsByClassName('menu-links')[0];
 const dimBackground = document.getElementsByClassName('dim-background')[0];
 const exitMobileButton = document.getElementsByClassName('exit-mobile-menu')[0];
 const menuTabs = document.getElementsByClassName('menutabs');
 
-toggleButton.addEventListener('click', () => {
+const toggleActiveClass = () => {
   navBarLinks.classList.toggle('active');
   dimBackground.classList.toggle('active');
   toggleButton.classList.toggle('active');
   exitMobileButton.classList.toggle('active');
-});
+};
 
-exitMobileButton.addEventListener('click', () => {
-  navBarLinks.classList.toggle('active');
-  dimBackground.classList.toggle('active');
-  toggleButton.classList.toggle('active');
-  exitMobileButton.classList.toggle('active');
-});
-
+toggleButton.addEventListener('click', toggleActiveClass);
+exitMobileButton.addEventListener('click', toggleActiveClass);
 for (let i = 0; i < menuTabs.length; i += 1) {
-  menuTabs[i].addEventListener('click', () => {
-    navBarLinks.classList.toggle('active');
-    dimBackground.classList.toggle('active');
-    toggleButton.classList.toggle('active');
-    exitMobileButton.classList.toggle('active');
-  });
+  menuTabs[i].addEventListener('click', toggleActiveClass);
 }
+// #endregion
